4da48dc7c6492bd9bd987b851890ab4c3d63d2b4
rithvikr <rithvik.redrouthu@gmail.com>
2024-07-13 19:04:18 -0400

    Install neo-tree

--- init.lua
+++ init.lua
@@ -31,10 +31,20 @@ require("lazy").setup({
   spec = {
     { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
     {
-      'nvim-telescope/telescope.nvim', tag = '0.1.8',
-      dependencies = { 'nvim-lua/plenary.nvim' }
+      "nvim-telescope/telescope.nvim", tag = "0.1.8",
+      dependencies = { "nvim-lua/plenary.nvim" }
     },
     { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+    {
+      "nvim-neo-tree/neo-tree.nvim",
+      branch = "v3.x",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
+        -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+      }
+    },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- automatically check for plugin updates
@@ -53,11 +63,13 @@ require("catppuccin").setup({
 
 vim.cmd.colorscheme "catppuccin"
 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+
+vim.keymap.set("n", "<leader>e", ":Neotree filesystem reveal left<CR>")
 
 local configs = require("nvim-treesitter.configs")
 configs.setup({
