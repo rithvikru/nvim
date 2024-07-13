9ab53ff4e2d00630a5fff19cbfce4eb1b4a42470
rithvikr <rithvik.redrouthu@gmail.com>
2024-07-13 18:41:50 -0400

    Install nvim-treesitter

--- init.lua
+++ init.lua
@@ -34,6 +34,7 @@ require("lazy").setup({
       'nvim-telescope/telescope.nvim', tag = '0.1.8',
       dependencies = { 'nvim-lua/plenary.nvim' }
     },
+    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- automatically check for plugin updates
