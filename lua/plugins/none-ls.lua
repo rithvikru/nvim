de5ecfd0e2f509d7bd622d4a33e70c72c900cb08
rithvikr <rithvik.redrouthu@gmail.com>
2024-07-13 23:46:57 -0400

    Add lsps to none-ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,7 +5,11 @@ return {
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.clang_format,
+        null_ls.builtins.formatting.prettier,
       },
     })
-  end
+
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  end,
 }
