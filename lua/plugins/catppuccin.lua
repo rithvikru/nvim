dfd040c9609dc40a495d8c09ab9e93939df91a2b
rithvikr <rithvik.redrouthu@gmail.com>
2024-07-14 11:54:14 -0400

    Make catppuccin pitch black theme

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,17 +1,18 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme "catppuccin"
-  end,
-  opts = {
-    color_overrides = {
-      mocha = {
-        base = "#000000",
-        mantle = "#000000",
-        crust = "#000000",
-      },
-    },
-  },
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	opts = {
+		color_overrides = {
+			mocha = {
+				base = "#000000",
+				mantle = "#000000",
+				crust = "#000000",
+			},
+		},
+	},
+	config = function(_, opts)
+		require("catppuccin").setup(opts)
+		vim.cmd.colorscheme("catppuccin")
+	end,
 }
