22a26a6a47a1c95a098122a5f2508beaced5022a
rithvikr <rithvik.redrouthu@gmail.com>
2024-07-14 12:21:20 -0400

    Change theme back to catppuccin

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,9 +1,10 @@
 return {
-  "nyoom-engineering/oxocarbon.nvim",
-  name = "oxocarbon",
-  priority = 1000,
-  config = function()
-    vim.opt.background = "dark"
-    vim.cmd.colorscheme("oxocarbon")
-  end,
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	opts = {},
+	config = function(_, opts)
+		require("catppuccin").setup(opts)
+		vim.cmd.colorscheme("catppuccin")
+	end,
 }
