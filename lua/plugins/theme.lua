7a45bae8ee14ce17a64a9b564373bcc6f4783b1c
rithvikr <rithvik.redrouthu@gmail.com>
2024-07-14 14:52:02 -0400

    Add nvim-dressing

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,10 +1,18 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
-	opts = {},
-	config = function(_, opts)
-		require("catppuccin").setup(opts)
-		vim.cmd.colorscheme("catppuccin")
-	end,
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {},
+    config = function(_, opts)
+      require("catppuccin").setup(opts)
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
+  {
+    {
+      "stevearc/dressing.nvim",
+      opts = {},
+    },
+  },
 }
