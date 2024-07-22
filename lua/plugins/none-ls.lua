a71bdc79045b4e0829f324526be4f3eca9aa2fc9
rithvikr <rithvik.redrouthu@gmail.com>
2024-07-22 13:14:55 -0400

    Remove unused formatters

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,6 +7,8 @@ return {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.clang_format,
         null_ls.builtins.formatting.prettier,
+        -- null_ls.builtins.formatting.ruff,
+        -- null_ls.builtins.formatting.rustfmt,
       },
     })
 
